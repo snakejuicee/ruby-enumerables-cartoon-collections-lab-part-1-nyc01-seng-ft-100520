aaa5934a4c0a2f96dd621ae28e2f0145588db879
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-27 01:01:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
 def greet_characters(array)
   array.each do |character|
     puts "Hello #{character}!"
+  end
 end
 
 def list_dwarves(array)
   array.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
+  end
 end
